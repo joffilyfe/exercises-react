40e54c8bc93714a1997b6c12fe8dee20a385b45f
Joffily Ferreira <joffilyferreira@gmail.com>
2020-08-30 18:20:22 -0300

    Provê contexto do Redux para os testes da aplicação

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
-test('renders the hello world h1', () => {
-  const { getByText } = render(<App />);
+const mockStore = configureStore();
+const store = mockStore({});
+
+test("renders the hello world h1", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App  />
+    </Provider>
+  );
   const linkElement = getByText(/Workout/i);
   expect(linkElement).toBeInTheDocument();
 });
