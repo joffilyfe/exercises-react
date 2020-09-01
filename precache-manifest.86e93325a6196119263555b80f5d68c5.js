a554c1773114406e2dc55ccea1acb4196e427ae1
Joffily Ferreira <joffilyferreira@gmail.com>
2020-09-01 20:47:56 -0300

    Demo v0.1.0

---
+++ precache-manifest.86e93325a6196119263555b80f5d68c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145b03795d7db0fec34263e63c345944",
+    "url": "/workoutlog/index.html"
+  },
+  {
+    "revision": "0fa0d825318bcf6b9e20",
+    "url": "/workoutlog/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "7759af676ac5a880720f",
+    "url": "/workoutlog/static/css/main.d15a0b03.chunk.css"
+  },
+  {
+    "revision": "0fa0d825318bcf6b9e20",
+    "url": "/workoutlog/static/js/2.d0f96abd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/workoutlog/static/js/2.d0f96abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7759af676ac5a880720f",
+    "url": "/workoutlog/static/js/main.20f668ac.chunk.js"
+  },
+  {
+    "revision": "fb15d03f54e8c0480c03",
+    "url": "/workoutlog/static/js/runtime-main.6b28e473.js"
+  }
+]);
